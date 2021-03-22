80068c8606a59240a636fc182f51e3f00750f1f3
ZairiJalal <zairi.jalal98@gmail.com>
2021-03-22 17:32:34 +0100

    DAO & API Task

--- src/main/java/org/sid/entities/Task.java
+++ src/main/java/org/sid/entities/Task.java
@@ -1,6 +1,7 @@
 package org.sid.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Data @AllArgsConstructor @NoArgsConstructor
 public class Task {
-	
+	@Id
 	private Long id;
 	private String name;
 
